8a4c2cdc36db9b4de4748e29e96e808f3d49d17e
darknebula05 <camms205@aol.com>
2024-10-22 18:53:06 +0000

    feat: move ssh pub keys to folder which get added to user and root

--- nix/nixosModules/user.nix
+++ nix/nixosModules/user.nix
@@ -31,23 +31,27 @@ with lib;
   };
 
   config = mkIf cfg.enable {
-    users = {
-      mutableUsers = false;
-      inherit (cfg) defaultUserShell;
-      users.${config.camms.variables.username} = {
-        isNormalUser = true;
-        uid = 1000;
-        extraGroups = optional (cfg.admin) "wheel" ++ cfg.extraGroups;
-        inherit (cfg) hashedPasswordFile;
+    users =
+      let
+        keys = mapAttrsToList (file: _: "${inputs.self}/keys/${file}") (
+          filterAttrs (_: t: t == "regular") (builtins.readDir "${inputs.self}/keys")
+        );
+      in
+      {
+        mutableUsers = false;
+        inherit (cfg) defaultUserShell;
+        users.${config.camms.variables.username} = {
+          isNormalUser = true;
+          uid = 1000;
+          extraGroups = optional (cfg.admin) "wheel" ++ cfg.extraGroups;
+          inherit (cfg) hashedPasswordFile;
+          openssh.authorizedKeys.keyFiles = keys;
+        };
+        users.root = {
+          hashedPassword = "$6$Wn08sMD6v9xuAkRA$KOEYdp9ZeyJ/FSxNZ9ViH6/qvZwbRQ5GZuEdMVdfjAIdprWlXN8XGaI/nJCc0ByHtiwhKcwem9BHWRGGWG6RB1";
+          openssh.authorizedKeys.keyFiles = keys;
+        };
       };
-      users.root = {
-        hashedPassword = "$6$Wn08sMD6v9xuAkRA$KOEYdp9ZeyJ/FSxNZ9ViH6/qvZwbRQ5GZuEdMVdfjAIdprWlXN8XGaI/nJCc0ByHtiwhKcwem9BHWRGGWG6RB1";
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJMumZUYRobSetz6wCJAIxryfbTUNf6fnGZU8P5RCcsW cameron@cam-desktop"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTr+IuOrJF+tNJHjwHaNTMnV1PwbvuO+Z1XeXXIETeq cshearer@nixos-wsl"
-        ];
-      };
-    };
     camms.user.hashedPasswordFile = mkIf sops (mkDefault config.sops.secrets.hashed_password.path);
     sops.secrets.hashed_password.neededForUsers = mkIf sops true;
   };
